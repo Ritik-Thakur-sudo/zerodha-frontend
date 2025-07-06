b9052804cd3db84e2cbd8300bc1ecce9ff1ecf67
Ritik Thakur <rk7495933@gmail.com>
2025-07-06 21:36:56 +0530

    Add ChargesTabs ServiceCharge components with their styleing file and  improve pricingPage and remove brokerege.js also improve some styles of pricingSection

--- src/landing_page/pricing/PricingPage.js
+++ src/landing_page/pricing/PricingPage.js
@@ -1,13 +1,23 @@
-import Hero from "../home/Hero";
-import Brokerege from "./Brokerege";
+import Hero from "./Hero";
 import OpenAccount from "../OpenAccount";
+import ChargesTabs from "./ChargesTabs";
+import ChargesExplainTab from "./ChargesExpainTab";
+import ServiceCharge from "./ServiceCharge"
 
 function PricingPage() {
   return (
     <>
       <Hero />
       <OpenAccount />
-      <Brokerege />
+      <ChargesTabs />
+      <p className="text-center fs-4">
+        <a className="text-decoration-none mx-1" href="#">
+          Calculate your costs upfront
+        </a>
+        using our brokerage calculator
+      </p>
+      <ChargesExplainTab / >
+      <ServiceCharge />
     </>
   );
 }
