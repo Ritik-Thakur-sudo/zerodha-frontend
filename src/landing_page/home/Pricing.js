08de657f60e85f22b54f5e4dd0a5d8dbab5e83c0
Ritik Thakur <rk7495933@gmail.com>
2025-07-31 00:09:17 +0530

    improve the stying of pricing.js along with pricing.css

--- src/landing_page/home/Pricing.js
+++ src/landing_page/home/Pricing.js
@@ -1,31 +1,46 @@
+import { Link } from "react-router-dom";
+
 import "../../css/Pricing.css";
 
 function Pricing() {
-    return ( 
-        <div className="container mb-5 p-5">
-            <div className="row">
-                <div className="col-5 mt-3">
-                    <h2 className="mb-3">Unbeatable pricing</h2>
-                    <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
-                    <a href="#" style={{textDecoration: "none"}}>See pricing <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
-                </div>
-                <div className=" main-price-box col-7 d-flex justify-content-center mt-5 mb-5">
-                    <div>
-                        <img src="media/images/pricing (1).svg" className="price"/>
-                        <p className="pricingTag1 text-muted"> Free account <br /> opening</p>
-                    </div>
-                    <div>
-                        <img src="media/images/pricing (1).svg" className="price"/>
-                        <p className="pricingTag2 text-muted">Free equity deliveryand <br /> direct mutual funds</p>
-                    </div>
-                    <div>
-                        <img src="media/images/pricing (2).svg" className="price"/>
-                        <p className="pricingTag3 text-muted"> Intraday and <br /> F&O</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container mb-5 p-5">
+      <div className="row">
+        <div className="col-5">
+          <h2>Unbeatable pricing</h2>
+          <p className="fs-6 mt-4">
+            We pioneered the concept of discount broking and price transparency
+            in India. Flat fees and no hidden charges.
+          </p>
         </div>
-     );
+        <div className="col d-flex justify-content-around align-items-center mt-5">
+          <div className="pricing-box">
+            <img src="media/images/pricing0.svg" alt="0 rupees" />
+            <p>
+              Free account <br /> opening
+            </p>
+          </div>
+          <div className="pricing-box">
+            <img src="media/images/pricing (1).svg" alt="0 rupess" />
+            <p>
+              Free equity delivery <br />
+              and direct mutual funds
+            </p>
+          </div>
+          <div className="pricing-box">
+            <img src="media/images/pricing (2).svg" alt="20 rupees" />
+            <p>
+              Intraday and <br />
+              F&O
+            </p>
+          </div>
+        </div>
+      </div>
+      <Link className="text-decoration-none" to={"/pricing"}>
+        See pricing <i class="fa fa-arrow-right" aria-hidden="true"></i>
+      </Link>
+    </div>
+  );
 }
 
 export default Pricing;
